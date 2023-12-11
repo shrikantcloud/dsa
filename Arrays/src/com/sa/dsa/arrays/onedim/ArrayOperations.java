9174b1d78a395f8b41a4a8b356855dfd7f724267
Shrikant Agiwal <shrikant.cloud@gmail.com>
2023-12-11 11:45:04 +0530

    Array Operations

--- Arrays/src/com/sa/dsa/arrays/onedim/ArrayOperations.java
+++ Arrays/src/com/sa/dsa/arrays/onedim/ArrayOperations.java
@@ -5,6 +5,17 @@ import java.util.stream.IntStream;
 
 public class ArrayOperations {
 
+    // Driver
+    public static void main(String[] args) {
+        ArrayOperations obj = new ArrayOperations();
+        int[] sample1DArr = obj.create();
+        obj.read(sample1DArr);
+        obj.update(sample1DArr, 5, 55);
+        obj.delete(sample1DArr, 5);
+        obj.delete(sample1DArr, 5);
+        obj.search(sample1DArr, 7);
+    }
+
     // Create
     public int[] create() {
         System.out.println("Creating 1D Array... ");
@@ -16,13 +27,13 @@ public class ArrayOperations {
     // Read
     public void read(int[] inputArr) {
         System.out.println("Reading Array...");
-        System.out.println("Input Array:"+Arrays.toString(inputArr)+", Size:"+ inputArr.length);
+        System.out.println("Input Array:" + Arrays.toString(inputArr) + ", Size:" + inputArr.length);
     }
 
     // Update
-    public void update(int[] inputArr, int position, int num){
+    public void update(int[] inputArr, int position, int num) {
         System.out.println("Updating array = updating number on a given position");
-        if (position > inputArr.length){
+        if (position > inputArr.length) {
             throw new IllegalArgumentException("Invalid Position!!");
         }
         inputArr[position] = num;
@@ -30,27 +41,28 @@ public class ArrayOperations {
         read(inputArr);
     }
 
-    public void delete(int[] inputArr, int position){
+    // Delete
+    public void delete(int[] inputArr, int position) {
         System.out.println("Deleting by Position of element...");
-        if (position > inputArr.length){
+        if (position > inputArr.length) {
             throw new IllegalArgumentException("Invalid Position!!");
         }
-        int[] modifiedArr =  IntStream.range(0, inputArr.length)
-                .filter(i -> i!= position)
-                .map( i -> inputArr[i])
+        int[] modifiedArr = IntStream.range(0, inputArr.length)
+                .filter(i -> i != position)
+                .map(i -> inputArr[i])
                 .toArray();
-        System.out.println("Element Deleted from position:"+position);
+        System.out.println("Element Deleted from position:" + position);
         read(modifiedArr);
     }
 
-    // Driver
-    public static void main(String[] args) {
-        ArrayOperations obj = new ArrayOperations();
-        int[] sample1DArr = obj.create();
-        obj.read(sample1DArr);
-        obj.update(sample1DArr, 5, 55);
-        obj.delete(sample1DArr, 5);
-        obj.delete(sample1DArr, 5);
+    // Search
+    public void search(int[] inputArr, int numToSearch) {
+        System.out.println("Searching... "+numToSearch);
+        for (int i = 0; i < inputArr.length; i++) {
+            if (inputArr[i] == numToSearch) {
+                System.out.println("Number found at position:" + i);
+                break;
+            }
+        }
     }
-
 }
