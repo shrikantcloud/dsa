601fced1e8937b30659ec72966e973aba74dffad
Shrikant Agiwal <shrikant.cloud@gmail.com>
2023-12-11 15:08:53 +0530

    refactored

---
+++ Arrays/src/com/sa/dsa/arrays/twodim/JaggedArray.java
@@ -0,0 +1,32 @@
+package com.sa.dsa.arrays.twodim;
+
+/*
+Array with unequal columns and Rows
+ */
+public class JaggedArray {
+
+    public static void main(String[] args) {
+        int row = 5;
+        int[][] jarr = new int[5][];
+
+        for (int i = 0; i < jarr.length; i++) {
+            jarr[i] = new int[i + 1];
+        }
+        int count = 0;
+        for (int i = 0; i < jarr.length; i++) {
+            for (int j = 0; j < jarr[i].length; j++) {
+                jarr[i][j] = count++;
+            }
+            System.out.println("Contents of 2D Jagged Array");
+        }
+
+        for (int i = 0; i < jarr.length; i++) {
+            for (int j = 0; j < jarr[i].length; j++) {
+                System.out.print(jarr[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
